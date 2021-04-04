1da26de87f789de86243529b24358b465836e799
vinay <vinay@DESKTOP-HP8PO56>
2021-04-04 18:59:43 -0400

    Test1 created

---
+++ GitProject/src/com/ingit/Test1.java
@@ -0,0 +1,18 @@
+package com.ingit;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		String hungry="yes";
+		
+		if(hungry=="yes") {
+			System.out.println("Go eat something");
+			
+		}else {
+			System.out.println("Do work");
+		}
+
+	}
+
+}
